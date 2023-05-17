15e61fd3ab3e198014d613895b197d9287986d03
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-17 09:55:55 +0530

    Add files via upload

---
+++ MVC Database/Data/MVCMoviesDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMoviesDatabase.Models;
+
+namespace MVCMoviesDatabase.Data
+{
+    public class MVCMoviesDatabaseContext : DbContext
+    {
+        public MVCMoviesDatabaseContext (DbContextOptions<MVCMoviesDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMoviesDatabase.Models.Movies> Movies { get; set; } = default!;
+    }
+}
